03df6e7908e43d3937c621be210cea6805437aa8
John Azuka <gooption@yahoo.com>
2022-04-20 19:47:46 +0200

    First Commit

--- routes/api.php
+++ routes/api.php
@@ -57,17 +57,40 @@ Route::post('articles', function(Request $request) {
 });
 
 Route::put('articles/{id}', function(Request $request, $id) {
-    if (Article::where('id', $id)->exists()) {
-       
-        $article = Article::findOrFail($id);
-        $article->update($request->all());
 
-        return $article;
-    } else {
+    $rules=array(
+        'title' => 'required|unique:articles|max:255',
+        'body' => 'required|max:4000',
+        'expired_at' => 'required',
+        'published_at' => 'required',
+    );
+    $messages=array(
+        'title.required' => 'Please enter a title.',
+        'body.required' => 'Please enter a body.',
+        'expired_at.required' => 'Please enter an expiration date.',
+        'published_at.required' => 'Please enter an publication date.'
+    );
+    $validator=Validator::make($request->all(),$rules,$messages);
+    if($validator->fails())
+    {
         return response()->json([
-        "message" => "Article not found"
-        ], 404);
+            "message" => "Validation was not successful"
+            ], 404); 
+    }else{
+        if (Article::where('id', $id)->exists()) {
+       
+            $article = Article::findOrFail($id);
+            $article->update($request->all());
+    
+            return $article;
+        } else {
+            return response()->json([
+            "message" => "Article not found"
+            ], 404);
+        }
+
     }
+    
 });
 
 Route::delete('articles/{id}', function($id) {
